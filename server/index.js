6c0ae527071ce87d77db9514c7bee83a8725b83e
Carlos Guzman <cags84@gmail.com>
2021-04-23 06:12:17 -0500

    Add middleware for not found route and errors

--- server/index.js
+++ server/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hola mundo');
+});
+
+// No route found
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
 });
 
 module.exports = app;
