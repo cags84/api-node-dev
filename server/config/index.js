ccdd04b4d9739db3f47663327d9ef11f2175f291
Carlos Guzman <cags84@gmail.com>
2021-04-23 05:07:31 -0500

    Changes minnor to config file

--- server/config/index.js
+++ server/config/index.js
@@ -4,8 +4,8 @@ dotEnv.config();
 
 const config = {
   server: {
-    hostname: process.env.SERVER_HOST,
-    port: process.env.SERVER_PORT,
+    hostname: process.env.SERVER_HOST || '127.0.0.1',
+    port: process.env.SERVER_PORT || 3000,
   },
 };
 
