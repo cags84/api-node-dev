3943d6cd023b72199ca2e6254fd78d1daaae93e2
Carlos Guzman <cags84@gmail.com>
2021-04-23 06:35:25 -0500

    Add winston to create a log system

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('./config/logger');
 
 const app = express();
 
@@ -6,9 +7,23 @@ app.get('/', (req, res, next) => {
   res.send('Hola mundo');
 });
 
+app.use((req, res, next) => {
+  const statusCode = 404;
+  const message = 'Route not found';
+
+  logger.warn(message);
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
+
 // No route found
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
+
+  logger.error(message);
 
   res.status(statusCode);
   res.json({
